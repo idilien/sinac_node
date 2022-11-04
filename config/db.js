e2f47266e121638b14564e7f0bed351c7dd09089
Javier <javiperez2010@yahoo.es>
2022-11-04 07:31:56 +0100

    sequelize setup

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv/config';
 
-const op = Sequelize.Op;
+const Op = Sequelize.Op
 const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
     host: process.env.DB_HOST,
     port: '3306',
@@ -9,21 +9,14 @@ const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_
     define: {
         timestamps: false
     },
+    operatorsAliases: Op,
     pool:{
         max:5,
         min:0,
         acquire:30000,
         idle: 10000
     },
-    operatorAliases: {
-        $and: op.and,
-        $or: op.or,
-        $eq: op.eq,
-        $gt: op.gt,
-        $lt: op.lt,
-        $lte: op.lte,
-        $like: op.like
-    }
+  
 });
 
 export default db;
