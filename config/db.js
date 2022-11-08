95284c97e6b0aac22a97022847ac86cf96e3f587
Javier <javiperez2010@yahoo.es>
2022-11-08 08:19:27 +0100

    v7

--- config/db.js
+++ config/db.js
@@ -6,21 +6,21 @@ const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_
     host: process.env.DB_HOST,
     port: '3306',
     dialect: 'mysql',
-    define: {
-        timestamps: false
-    },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    pool:{
-      max:5,
-      min:0,
-      acquire:30000,
-      idle: 10000
-    },
+    // define: {
+    //     timestamps: false
+    // },
+    // dialectOptions: {
+    //   ssl: {
+    //     require: true,
+    //     rejectUnauthorized: false
+    //   }
+    // },
+    // pool:{
+    //   max:5,
+    //   min:0,
+    //   acquire:30000,
+    //   idle: 10000
+    // },
     
     
   });
